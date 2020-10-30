6808fc927ccab791f3eaaca461773e81de1038c3
okafohenrie <okafohenrie@gmail.com>
2020-10-30 13:26:01 +0100

    fixed code to compare passwords in script.js, made slight modifications to index.css

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 		let signUpBtn = document.getElementById("sign-up-btn");
-		if((document.getElementById("first-name").value || document.getElementById("last-name").value || document.getElementById("email").value  || document.getElementById("password").value || document.getElementById("conf-password").value) == null && document.getElementById('checkbox').checked == false ){
+		let firstName = document.getElementById("first-name").value;
+		let lastName = document.getElementById("last-name").value;
+		let email = document.getElementById("email").value;
+		let password = document.getElementById("password").value;
+		let confPassword = document.getElementById("conf-password").value;
+		let checkbox = document.getElementById('checkbox');
+		if((firstName || lastName  || email || password || confPassword ) === "" ){
 			signUpBtn.disabled = true;
 			signUpBtn.style.backgroundColor = "#f2f2f2";
 			signUpBtn.style.cursor = "not-allowed";
-		}else{
+		}else if((firstName && lastName  && email && password && confPassword ) !== "" ){
 			signUpBtn.disabled = false;
 			signUpBtn.style.backgroundColor = "#D3AF41";
 			signUpBtn.style.cursor = "pointer";
@@ -17,7 +23,7 @@
 			let firstName = document.getElementById('first-name').value;
 			let lastName = document.getElementById('last-name').value;
 			if(regEx.test(firstName) || regEx.test(lastName)){
-				document.getElementById("name-error").style.display = "block"
+				document.getElementById("name-error").style.display = "block";
 			}else{
 				document.getElementById("name-error").style.display = "none"
 			}
@@ -37,8 +43,9 @@
 			}else{				
 				document.getElementById("password-error").style.display = "none";
 			}
+
 			// confirm password validation
-			if(document.getElementById('password').value !== document.getElementById('conf-password').value){
+			if(document.getElementById("password").value !== document.getElementById("conf-password").value){
 				document.getElementById("conf-password-error").style.display = "block";
 			}
 		}
